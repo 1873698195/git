af0d21be4cb253e80cdb5a1fd1f05c737cd779fb
chenruifeng <1873698195@qq.com>
2021-12-03 23:24:48 +0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * ClassName:Test
+ * Description:
+ *
+ * @Date:2021/12/3 23:24
+ * @Author:chenruifeng
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git hello");
+    }
+}
